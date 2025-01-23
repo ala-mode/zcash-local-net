c79a4809cd7ed4ae7adbc6f72ce36703d4115e7e
al amoda <alamoda@tutamail.com>
2025-01-23 04:10:50 -0500

    modify utils/ scripts

--- utils/generate_chain_caches.sh
+++ utils/generate_chain_caches.sh
@@ -1,3 +1,5 @@
 set -e -x
+rm -rf ./chain_cache/
+cargo clean
 cargo nextest run generate_zcashd_chain_cache --run-ignored ignored-only
 cargo nextest run generate_zebrad_large_chain_cache --run-ignored ignored-only
--- utils/testme.sh
+++ utils/testme.sh
@@ -1,2 +1,1 @@
 cargo nextest run --features test_fixtures
-
