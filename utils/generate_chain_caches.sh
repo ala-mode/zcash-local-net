5e172d08d89f444321563710ad6cd8881ccb33bf
al amoda <alamoda@tutamail.com>
2025-01-26 04:42:38 -0500

    add logging for timing of tests

--- utils/generate_chain_caches.sh
+++ utils/generate_chain_caches.sh
@@ -2,4 +2,10 @@ set -e -x
 rm -rf ./chain_cache/
 cargo clean
 cargo nextest run generate_zcashd_chain_cache --run-ignored ignored-only
+echo "after generate_zcashd_chain_cache" >> log_test_times.txt
+date -d @$(date +%s) >> log_test_times.txt
+
 cargo nextest run generate_zebrad_large_chain_cache --run-ignored ignored-only
+echo "after generate_zebrad_large_chain_cache" >> log_test_times.txt
+date -d @$(date +%s) >> log_test_times.txt
+
--- utils/reset_generate_testme.sh
+++ utils/reset_generate_testme.sh
@@ -1,3 +1,10 @@
 set -e -x
+echo "before generate_chain_caches.sh :" > log_test_times.txt
+date -d @$(date +%s) >> log_test_times.txt
 ./utils/generate_chain_caches.sh
+echo "after generate_chain_caches.sh :" >> log_test_times.txt
+date -d @$(date +%s) >> log_test_times.txt
 cargo clean && cargo nextest run --features test_fixtures
+echo "after cargo nextest run :" >> log_test_times.txt
+date -d @$(date +%s) >> log_test_times.txt
+
